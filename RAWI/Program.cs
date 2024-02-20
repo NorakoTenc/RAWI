06b6a15ff9950b4131ad42a8b4f877b806decbc7
NorakoTenc <worldhentai69@outlook.com>
2024-02-20 23:29:06 +0200

    1

--- RAWI/Program.cs
+++ RAWI/Program.cs
@@ -1,25 +1,15 @@
-﻿class RAWI
+﻿class Program
 {
-    static void Main(string[] args)
-    {
-        Thread threadMethod1 = new Thread(Sleep3k);
-        threadMethod1.Start();
-        Thread threadMethod2 = new Thread(Sleep2k);
-        threadMethod2.Start();
-        Sleep4kAsync().Wait();
-    }
-
     static void Sleep3k()
     {
         Console.WriteLine("Sleep1: " + Thread.CurrentThread.ManagedThreadId);
         for (int i = 0; i < 1000000000; i++)
         {
-            if (i%2==0)
-            Math.Sqrt(i);
+            if (i % 2 == 0)
+                Math.Sqrt(i);
         }
         Console.WriteLine("Sleep1 passed.");
     }
-
     static void Sleep2k()
     {
         Console.WriteLine("Sleep2k: " + Thread.CurrentThread.ManagedThreadId);
@@ -31,5 +21,13 @@
         Console.WriteLine("Sleep4kAsync: " + Thread.CurrentThread.ManagedThreadId);
         await Task.Delay(4000);
         Console.WriteLine("4k passed asyncronacly .");
+    }
+    static void Main(string[] args)
+    {
+        Thread threadMethod1 = new Thread(Sleep3k);
+        threadMethod1.Start();
+        Thread threadMethod2 = new Thread(Sleep2k);
+        threadMethod2.Start();
+        Sleep4kAsync().Wait();
     }
 }
